f42e3865c5e00256f1a1c566f7b1cd910070ef35
Calixto Rodrigues Macedo <calixto.macedo@gmail.com>
2024-05-19 16:37:38 -0300

    Melhoria no time

--- src/main/java/br/com/cmdev/learning/apachecamel/routers/TimerRouter.java
+++ src/main/java/br/com/cmdev/learning/apachecamel/routers/TimerRouter.java
@@ -8,13 +8,11 @@ public class TimerRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-/*
-        from("timer:myFirstTimerCamel?period=5000")
+        from("timer:myFirstTimerCamel?repeatCount=1&delay=10000")
                 .routeId("my-first-timer-id")
                 .log("Data e hora agora: ${date:now:dd/MM/yyyy HH:mm:ss}")
                 .end();
 
- */
     }
 
 
